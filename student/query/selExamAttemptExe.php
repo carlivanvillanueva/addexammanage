69327f93a5213e8d6ff4b50206ea8d1759181149
carlivanvillanueva <105845802+carlivanvillanueva@users.noreply.github.com>
2023-08-10 16:05:44 +0800

    Add files via upload

---
+++ student/query/selExamAttemptExe.php
@@ -0,0 +1,21 @@
+<?php 
+$student_id = $_SESSION['student_id'];
+include '../DB_connection.php';
+
+extract($_POST);
+
+ $selExamAttmpt = $conn->query("SELECT * FROM exam_attempt WHERE student_id='$student_id' AND exam_id='$thisId' ");
+
+ if($selExamAttmpt->rowCount() > 0)
+ {
+ 	$res = array("res" => "alreadyExam", "msg" => $thisId);
+ }
+ else
+ {
+ 	$res = array("res" => "takeNow");
+ }
+
+
+ echo json_encode($res);
+
+ ?>
